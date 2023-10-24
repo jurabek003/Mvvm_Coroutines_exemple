ceebaefda051c7b6229fd2301833e9b230f65611
Asus <turgunboyevjurabek777@gmail.com>
2023-10-24 14:51:03 +0500

    Initial commit

---
+++ app/src/main/java/uz/turgunboyevjurabek/mvvmcoroutinesexemple/utils/Resourse.kt
@@ -0,0 +1,16 @@
+package uz.turgunboyevjurabek.mvvmcoroutinesexemple.utils
+
+data class Resourse<out T>(val status: Status,val date:T?,val massage:String?){
+    companion object{
+        fun <T>success(data:T):Resourse<T>{
+            return Resourse(Status.SUCCESSFUL,data,null)
+        }
+        fun <T>error(massage: String?):Resourse<T>{
+            return Resourse(Status.ERROR,null,massage)
+        }
+        fun <T>loading(massage: String?):Resourse<T>{
+            return Resourse(Status.LOADING,null,null)
+        }
+        
+    }
+}
---
+++ app/src/main/java/uz/turgunboyevjurabek/mvvmcoroutinesexemple/utils/Status.kt
@@ -0,0 +1,7 @@
+package uz.turgunboyevjurabek.mvvmcoroutinesexemple.utils
+
+enum class Status {
+    LOADING,
+    ERROR,
+    SUCCESSFUL
+}
