cbb93418391bc4323970d5479f72bbe28608eab7
Asus <turgunboyevjurabek777@gmail.com>
2023-10-25 23:30:47 +0500

    DataBase qushildi

--- app/src/main/java/uz/turgunboyevjurabek/mvvmcoroutinesexemple/madels/Post_Client.kt
+++ app/src/main/java/uz/turgunboyevjurabek/mvvmcoroutinesexemple/madels/Post_Client.kt
@@ -2,19 +2,12 @@ package uz.turgunboyevjurabek.mvvmcoroutinesexemple.madels
 
 
 import androidx.room.Entity
-import androidx.room.PrimaryKey
 import com.google.gson.annotations.SerializedName
+
 @Entity
-data class Get_AllClients(
-    @SerializedName("client_rasm")
-    var clientRasm: String?,
+data class Post_Client(
     @SerializedName("fam")
     val fam: String,
-
-    @PrimaryKey(autoGenerate = true)
-    @SerializedName("id")
-    val id: Int,
-
     @SerializedName("ism")
     val ism: String,
     @SerializedName("manzil")
@@ -23,4 +16,6 @@ data class Get_AllClients(
     val tel: String,
     @SerializedName("umumiy_summa")
     val umumiySumma: Int
+
+
 )
